f7d0d3214dee330a2e9aedf36892d6720a713387
user <user@40005-PC0WQA15>
2025-10-06 23:03:32 +0700

    kirana commit

--- Scripts/Sign In with Email/Script1759726281554.groovy
+++ Scripts/Sign In with Email/Script1759726281554.groovy
@@ -78,6 +78,3 @@ WebUI.setEncryptedText(findTestObject('Object Repository/Page_Fotogrit/input__pa
 WebUI.click(findTestObject('Object Repository/Page_Fotogrit/input__robot'))
 WebUI.click(findTestObject('Object Repository/Page_Fotogrit/input__w-full h-44px rounded-md text-white _3c447d'))
 WebUI.delay(15)
-
-
-Coba aku nambahin code baru disini
